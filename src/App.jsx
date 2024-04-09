3891870bbdab84bbc42e85f63674c5dcb4774ef2
Mehmet Elifoğlu <mehmeteoglutr@gmail.com>
2024-04-09 04:00:35 +0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -377,7 +377,7 @@ function App() {
 
             <div>
               <button className="hover:shadow-form w-full rounded-md bg-[#6A64F1] py-3 px-8 text-center text-base font-semibold text-white outline-none">
-                Book Appointment
+               Gönder
               </button>
             </div>
           </form>
